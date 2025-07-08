ee9bef148809b47c0d7238e4a24684a817d33ab9
hp <hp@Rajat>
2025-07-08 16:34:17 +0530

    Challenge 1

--- LearningATB13xProgram/src/Java_Programming_Assessment/Challenge01.java
+++ LearningATB13xProgram/src/Java_Programming_Assessment/Challenge01.java
@@ -1,15 +1,22 @@
 package Java_Programming_Assessment;
+import java.util.*;
 
 public class Challenge01 {
 	public static void main(String[]args)
 	{
-		int Integer = 25;
-		double digit  = 3.14;
-		boolean r = true;
 		
-		System.out.println(Integer);
-		System.out.println(digit);
-		System.out.println(r);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int Integer = sc.nextInt();
+		System.out.println("Enter a double");
+		double digit = sc.nextDouble();
+		System.out.println("Enter a char");
+		boolean r = sc.hasNext();
+		
+		
+		System.out.print("Integer: " + Integer + " ");
+		System.out.print("Double: " + digit + " ");
+		System.out.print("Boolean: " + r);
 	
 	}
 	}

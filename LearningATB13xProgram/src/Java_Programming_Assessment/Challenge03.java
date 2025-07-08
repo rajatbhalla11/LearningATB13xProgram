fefa4a294fa6c3dcb8a773673d91be61e645fb16
hp <hp@Rajat>
2025-07-08 17:43:39 +0530

    Challenge 3

---
+++ LearningATB13xProgram/src/Java_Programming_Assessment/Challenge03.java
@@ -0,0 +1,49 @@
+package Java_Programming_Assessment;
+import java.util.*;
+public class Challenge03 {
+		
+
+		public static void main(String[]args)
+		{
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter first number");
+			int a = sc.nextInt();
+			System.out.println("Enter second number");
+			int b = sc.nextInt();
+			
+			// Arithmetic Operations
+	        int addition = a + b;
+	        int subtraction = a - b;
+	        int multiplication = a * b;
+	        int division = a / b;
+	        int modulus = a % b;
+
+	        System.out.println("Addition: " + addition);
+	        System.out.println("Subtraction: " + subtraction);
+	        System.out.println("Multiplication: " + multiplication);
+	        System.out.println("Division: " + division);
+	        System.out.println("Modulus: " + modulus);
+
+	        // Assignment Operations
+	        int x = a; // copy of a for demonstration
+	        x += b;
+	        System.out.println("After x += b: " + x);
+
+	        x = a;
+	        x -= b;
+	        System.out.println("After x -= b: " + x);
+
+	        x = a;
+	        x *= b;
+	        System.out.println("After x *= b: " + x);
+
+	        x = a;
+	        x /= b;
+	        System.out.println("After x /= b: " + x);
+
+	        x = a;
+	        x %= b;
+	        System.out.println("After x %= b: " + x);
+		}
+		}

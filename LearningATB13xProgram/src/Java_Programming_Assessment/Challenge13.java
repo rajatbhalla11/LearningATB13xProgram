82ebb41519b705db805052382408ec9fb757769d
hp <hp@Rajat>
2025-07-15 00:25:05 +0530

    Challenge13

---
+++ LearningATB13xProgram/src/Java_Programming_Assessment/Challenge13.java
@@ -0,0 +1,17 @@
+package Java_Programming_Assessment;
+
+import java.util.Scanner;
+import java.util.*;
+public class Challenge13 {
+	public static void main(String[]args)
+	{	
+		for(int i =1;i<=5;i++)
+		{
+			for(int j = 1;j<=10;j++)
+			{
+				int result = i*j;
+				System.out.println("Table of i: " + i + "x" + i + "=" + result);
+			}
+		}
+	}
+}

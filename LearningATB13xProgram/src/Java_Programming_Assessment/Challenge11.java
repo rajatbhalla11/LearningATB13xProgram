ac48e17ada2a15e9a1815f4516b88394cbeabe0d
hp <hp@Rajat>
2025-07-14 20:43:23 +0530

    Challenge 11

---
+++ LearningATB13xProgram/src/Java_Programming_Assessment/Challenge11.java
@@ -0,0 +1,16 @@
+package Java_Programming_Assessment;
+import java.util.*;
+public class Challenge11 {
+	public static void main(String[]args)
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a random number");
+		int n =sc.nextInt();
+		
+		for(int i =1;i<=n;i++)
+		{
+			System.out.print(i + " ");
+		}
+	}
+
+}

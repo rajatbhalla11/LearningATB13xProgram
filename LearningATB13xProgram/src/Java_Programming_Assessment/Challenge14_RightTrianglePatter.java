484631de4809478f2a465b8c3cebe27c411440ab
hp <hp@172.20.22.99>
2025-07-15 15:37:22 +0530

    Challenge14_RightTrianglePattern

--- LearningATB13xProgram/src/Java_Programming_Assessment/Challenge14_RightTrianglePatter.java
+++ LearningATB13xProgram/src/Java_Programming_Assessment/Challenge14_RightTrianglePatter.java
@@ -7,11 +7,8 @@ public class Challenge14_RightTrianglePatter {
 	public static void main(String[]args)
 	{
 	        Scanner sc = new Scanner(System.in);
-			System.out.println("Enter a number of rows");
+			System.out.println("Enter the number of rows");
 			int m =sc.nextInt();
-			System.out.println("Enter a number of columns");
-			int n =sc.nextInt();
-			
 			for(int i = 1;i<=m;i++)
 			{
 				for(int j = 1;j <=i;j++)
